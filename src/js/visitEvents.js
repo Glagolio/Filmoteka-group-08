15ea7c2dfa396c46a5976e4c00ded484217f02ad
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-07-19 16:20:45 +0300

    remove-visit-close-button

--- src/js/visitEvents.js
+++ src/js/visitEvents.js
@@ -3,14 +3,12 @@ import { storagePage } from './pageInStorage';
 
 const refs = {
     comeBackButton: document.querySelector('button.js-visit-back-btn'),
-    visitModal: document.querySelector("[data-modal].dialog"),
-    visitClose: document.querySelector("button.js-dialog-close"),
+    visitModal: document.querySelector("[data-modal].dialog"),   
     visitCloseBtn: document.querySelector("button.js-visit-close-btn"),
 };
 
 
-    refs.comeBackButton.addEventListener("click", loadStoragePage);
-    refs.visitClose.addEventListener("click", toggleModal);
+    refs.comeBackButton.addEventListener("click", loadStoragePage);    
     refs.visitCloseBtn.addEventListener("click", toggleModal);
     
 (() => {
@@ -32,8 +30,7 @@ function toggleModal() {
     
     if (refs.visitModal.classList.value.includes("hidden") === true) {
            clearTimeout(timerId);  
-        refs.comeBackButton.removeEventListener("click", loadStoragePage);
-        refs.visitClose.removeEventListener("click", toggleModal);
+        refs.comeBackButton.removeEventListener("click", loadStoragePage);        
         refs.visitCloseBtn.removeEventListener("click", toggleModal);
     };
 };
